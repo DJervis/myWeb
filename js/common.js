1418a3acaf7f8273900a9b7ee4990e783f3ec24a
dengjie <dengjie@16fan.com>
2015-02-03 18:35:55 +0800

    maopao

--- js/common.js
+++ js/common.js
@@ -109,3 +109,12 @@ function scrollToS(end_height, stime){ //滚动函数（高度，时间）
         }
     },1);
 }
+/*--------------------------- 阻止冒泡 ---------------------------------*/
+function stopPropagation(e) {
+    e = e || window.event;
+    if(e.stopPropagation) { //W3C阻止冒泡方法
+        e.stopPropagation();
+    } else {
+        e.cancelBubble = true; //IE阻止冒泡方法
+    }
+}
