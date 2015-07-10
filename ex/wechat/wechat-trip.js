29c6b63bc42a2c637972ae3bd4146ba070d410a3
dengjie <djdengjie@126.com>
2015-07-10 18:01:52 +0800

    wechat

--- ex/wechat/wechat-trip.js
+++ ex/wechat/wechat-trip.js
@@ -11,10 +11,12 @@
 	}, 0);
 };*/
 var score = 0;
-var rightAnswer = {
-	1: "A",
-	2: "B",
-	3: "C"
+var rightAnswer = [0, 1, 3, 0, 2, 0, 1, 1, 3, 2];
+var descrip = {
+	"0-30" : "对于一个不爱好旅游的人来说，这已经实属不易，旅行前请一定做好旅行攻略",
+	"40-60" : "虽然分数不高，但你是一个值得信赖的旅行小伙伴，请再接再厉！",
+	"70-90" : "分数不错！一个人旅行也不用担心找不到好吃的好玩的。",
+	"100" : "你就是我们要找的旅行控！跟你一同出游，可以放心玩！"
 };
 $(function() {
     FastClick.attach(document.body);
@@ -33,7 +35,7 @@ $(function() {
     	select();
     	setTimeout(function(){
     		startPage.css('display', 'none');
-    	}, 400);
+    	}, 500);
     });
     // next question    
     nextBtn.on('click', function(){
@@ -52,10 +54,12 @@ $(function() {
     			nextPage.addClass('active');
     			if(n == 9) {
     				sharePage.css('display', 'block');
+    				nextBtn.text('完成');
     			}
     		} else if(n == 10) {
     			sharePage.addClass('active');
     			nextBtn.fadeOut('fast');
+    			getScore();
     		}  		
     		if((n-1) >= 1){
     			prePage.css('display', 'none');
@@ -69,7 +73,7 @@ $(function() {
     			active.css('display', 'none');
     			active.find('.question li').off('click');
     			select();
-    		}, 400);	
+    		}, 500);	
     	}
     });
 
@@ -84,5 +88,60 @@ $(function() {
 	    		nextBtn.removeClass('disable');
 	    	}
 	    });
-    }       
+    }   
+
+    //get score
+    function getScore(){
+    	var selectList = $('.question .selected');
+    	var selectArr = [];
+    	var description = $('.description');
+    	for (var i=0; i<selectList.length; i++){
+    		selectArr[i] = $(selectList[i]).index();
+    		if(rightAnswer[i] == selectArr[i]){
+    			score += 10;
+    		}
+    	}
+    	console.log(selectArr);
+    	$('#score').text(score);
+    	if( score <= 30){
+    		description.text(descrip['0-30']);
+    	}else if(score > 30 && score <= 60){
+    		description.text(descrip['40-60']);
+    	}else if(score > 60 && score <= 90){
+    		description.text(descrip['70-90']);
+    	}else if(score == 100){
+    		description.text(descrip['100']);
+    	}
+    }  
+
+    //reload 
+    $('.reload').on('click', function(){
+    	window.location.reload();
+    });
+    $('#reload').on('click', function(){
+    	window.location.reload(true);
+    });
 });
+!function(){
+	var downBtn = $('.download');
+	var u=navigator.userAgent;
+    var android = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1;
+    var isweixin = function(){
+        var ua = navigator.userAgent.toLowerCase();
+        if(ua.match(/MicroMessenger/i)=="micromessenger") {
+            return true;
+        } else {
+            return false;
+        }
+    }();
+
+    if(android){
+        downBtn.attr("href","http://www.16fan.com/o/Fan16.apk");
+    }
+    if (isweixin) {
+        downBtn.attr({
+            href: "http://a.app.qq.com/o/simple.jsp?pkgname=com.fan.app&g_f=991653",
+            onclick: "javascript:_gaq.push(['_trackPageview','weixin-game']);"
+        });
+    };
+}();
