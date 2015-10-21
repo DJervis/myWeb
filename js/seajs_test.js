74e1135a3b436d38d33d8c4919a6f2f0df1e002e
dengjie <djdengjie@126.com>
2015-10-21 17:51:57 +0800

    sea

--- js/seajs_test.js
+++ js/seajs_test.js
@@ -1,8 +1,26 @@
 define(function(require, exports, module){	
+    var $ = require('jquery');
 	module.exports = {
 		"say": say
 	};
 	function say(str){
-		alert(str);
+		alert('say: '+str);
 	}
+});
+
+seajs.use('seajs_test', function(test){
+    // test.say();
+    var btn = $('.btn');
+    for(var i=0; i<btn.length; i++){
+        btn[i].onmouseover = function(num){
+            return function(){
+                test.say('mousevoer '+num);
+            };
+        }(i);
+        btn[i].onclick = function(num){
+            return function(){
+                test.say('click '+num);
+            };
+        }(i);
+    }
 });
